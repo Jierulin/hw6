71138a37407ba0edbe81c6270a01ae6af0b43fef
Jierulin <31756333+Jierulin@users.noreply.github.com>
2017-10-15 05:34:57 +0800

    Update Frank Stella-sketch 3.js

--- Frank Stella-sketch 3.js
+++ Frank Stella-sketch 3.js
@@ -1,1 +1,18 @@
+function setup() { 
+  createCanvas(410, 410);
+	colorMode(HSB);
+ background(0);
+} 
 
+function draw() { 
+
+	stroke(255);
+for (x=0,y=410,h=250,b=40;x<200;x=x+10,y=y-20,h=h-18,b=b+3){
+
+	strokeWeight(5);
+	stroke(h,100,b)
+	rect(x,x,y,y)
+}
+
+
+}
